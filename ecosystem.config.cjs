4923f78efdc393d83a950779eb80a096b4e65437
alishercpp <alisher.abdimuminov.2005@gmail.com>
2025-02-18 11:20:39 +0500

    bug fix

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
         name: 'Faceium Admin',
         port: '3001',
         exec_mode: 'cluster',
-        instances: 'max',
+        instances: 1,
         script: './.output/server/index.mjs'
       }
     ]
